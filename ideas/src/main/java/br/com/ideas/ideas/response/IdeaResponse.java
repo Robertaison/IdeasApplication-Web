697d8035b0b37d706040a8a683dc34e25076ebb2
Robertson Souza <roberts.sbeer@gmail.com>
2018-11-05 15:43:08 -0200

    Inicializando projeto

---
+++ ideas/src/main/java/br/com/ideas/ideas/response/IdeaResponse.java
@@ -0,0 +1,4 @@
+package br.com.ideas.ideas.response;
+
+public class IdeaResponse {
+}
---
+++ ideas/src/main/java/br/com/ideas/ideas/service/IdeaService.java
@@ -0,0 +1,7 @@
+package br.com.ideas.ideas.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class IdeaService{
+}

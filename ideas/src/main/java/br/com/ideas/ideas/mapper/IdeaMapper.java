76e39c2a119004289e88d5a7532b3137cf4a9646
Robertson Souza <roberts.sbeer@gmail.com>
2018-11-08 14:18:58 -0200

    Arrumando responsividade do front e sem js, back arrumando conexao com banco

--- ideas/src/main/java/br/com/ideas/ideas/mapper/IdeaMapper.java
+++ ideas/src/main/java/br/com/ideas/ideas/mapper/IdeaMapper.java
@@ -1,8 +1,5 @@
 package br.com.ideas.ideas.mapper;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.Mapping;
-
 public class IdeaMapper {
 
 

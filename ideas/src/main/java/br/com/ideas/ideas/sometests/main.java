a1188aa9d4e624b10f8954784ecdefdab2c490a2
Robertson <s2it_rsouza@uolinc.com>
2018-11-08 14:18:58 -0200

    Arrumando responsividade do front e sem js, back arrumando conexao com banco

--- ideas/src/main/java/br/com/ideas/ideas/sometests/main.java
+++
@@ -1,9 +0,0 @@
-package br.com.ideas.ideas.sometests;
-
-
-public class main {
-    public static void main (String[] args){
-        DateNow data = new DateNow();
-        System.out.println(data.getDateTime());
-    }
-}

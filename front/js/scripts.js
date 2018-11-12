e163506b4e1cac8e9da7951aa24b1a01e99ff0c7
Robertson <s2it_rsouza@uolinc.com>
2018-11-12 15:43:47 -0200

    Criação do modal e autorização do CORS para requisição

--- front/js/scripts.js
+++ front/js/scripts.js
@@ -1,18 +1,53 @@
-var request = new XMLHttpRequest();
+// var request = new XMLHttpRequest();
 
-request.open('GET', 'http://localhost:8080/', true);
-request.onload = function () {
+// request.open('GET', 'http://localhost:8080/', true);
 
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
+// request.onload = function () {
 
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(thought => {
-      console.log(thought.title);
-    });
-  } else {
-    console.log('error');
-  }
+//   // Begin accessing JSON data here
+//   var data = JSON.parse(this.response);
+
+//   if (request.status >= 200 && request.status < 400 && request.status != 500) {
+//     data.forEach(thought => {
+//       console.log(thought.title);
+//     });
+//   } else {
+//     console.log('error');
+//   }
+// }
+
+// request.send();
+
+async function getElements(){
+  const res = await fetch('http://localhost:8080/');
+  const data = await res.json();
+  console.log(data);
 }
 
-request.send();
+getElements();
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
--- thoughts/src/main/java/br/com/thoughts/thoughts/controller/ThoughtController.java
+++ thoughts/src/main/java/br/com/thoughts/thoughts/controller/ThoughtController.java
@@ -24,6 +24,7 @@ public class ThoughtController {
     }
 
     @GetMapping
+    @CrossOrigin("*")
     public List<Thought> findAll() {
         return thoughtService.retrieveAllThoughts();
     }

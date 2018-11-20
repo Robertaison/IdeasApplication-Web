73173615502d3b1b2f1a963533a31f8bdae5f2d6
Robertaison <roberts.sbeer@gmail.com>
2018-11-20 21:29:58 -0200

    Pequenos ajustes responsividade

--- front/js/scripts.js
+++ front/js/scripts.js
@@ -21,7 +21,7 @@
 async function getElements(){
   const res = await fetch('http://localhost:8080/');
   const data = await res.json();
-  const dados = data[1].description;
+  const dados = data[0].description;
 
   var teste = document.getElementById("aqui");
   var a = document.createTextNode(dados);
@@ -29,7 +29,7 @@ async function getElements(){
 }
 
 async function getElementsById(){
-    const res = await fetch('http://localhost:8080/2');
+    const res = await fetch('http://localhost:8080/1');
     const data = await res.json();
     const dados = data;
     
@@ -39,53 +39,58 @@ async function getElementsById(){
     // teste.appendChild(a);
 }
 
-function myFunction() {
-    modal.style.display = "block";
-}
+// function myFunction() {
+//     modal.style.display = "block";
+// }
 
 getElementsById();
 getElements();
 
-// // Get the modal
-// var modal = document.getElementById('myModal');
+// Get the modal
+var modal = document.getElementById('myModal');
+var modalBaloon = document.getElementById('myBaloonModal');
 
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+var btnBaloon = document.getElementById("baloonModal");
 
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-// var aqui = document.getElementById("aqui");
-// aqui.appendChild(getElements);
+var aqui = document.getElementById("aqui");
+aqui.appendChild(getElements);
 
-// // When the user clicks on the button, open the modal 
-// btn.onclick = function() {
-//     modal.style.display = "block";
-// }
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
 
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//     modal.style.display = "none";
-// }
+btnBaloon.onclick = function(){
+	modalBaloon.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
 
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
-// //modal auto-expand
-// $(document)
-//     .one('focus.autoExpand', 'textarea.autoExpand', function(){
-//         var savedValue = this.value;
-//         this.value = '';
-//         this.baseScrollHeight = this.scrollHeight;
-//         this.value = savedValue;
-//     })
-//     .on('input.autoExpand', 'textarea.autoExpand', function(){
-//         var minRows = this.getAttribute('data-min-rows')|0, rows;
-//         this.rows = minRows;
-//         rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 16);
-//         this.rows = minRows + rows;
-//     });
+//modal auto-expand
+$(document)
+    .one('focus.autoExpand', 'textarea.autoExpand', function(){
+        var savedValue = this.value;
+        this.value = '';
+        this.baseScrollHeight = this.scrollHeight;
+        this.value = savedValue;
+    })
+    .on('input.autoExpand', 'textarea.autoExpand', function(){
+        var minRows = this.getAttribute('data-min-rows')|0, rows;
+        this.rows = minRows;
+        rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 16);
+        this.rows = minRows + rows;
+    });

7d3e33b4ee2a2b6fad7042673a76bd2c4825870c
Robertaison <roberts.sbeer@gmail.com>
2018-11-21 02:34:25 -0200

    Trabalhando em post

--- front/js/scripts.js
+++ front/js/scripts.js
@@ -20,21 +20,23 @@
 
 // getAllElements
 async function getElements(){
-  const res = await fetch('http://localhost:8080/');
-  const data = await res.json();
-  const dados = data[0].description;
+	const res = await fetch('http://localhost:8080/');
+	const data = await res.json();
+	const dados = data[0].description;
 
-  var teste = document.getElementById("aqui");
-  var a = document.createTextNode(dados);
-  teste.appendChild(a);
+	var teste = document.getElementById("aqui");
+	var a = document.createTextNode(dados);
+	teste.appendChild(a);
 }
 
 async function getElementsById(){
-    const res = await fetch('http://localhost:8080/1');
-    const data = await res.json();
-    const dados = data;
-    
-    console.log(dados);
+	const res = await fetch('http://localhost:8080/2');
+	const data = await res.json();
+	const dados = data.title;
+	var placeHolder = document.createTextNode(dados);
+
+	var titleBaloon = document.getElementById("title").placeholder = placeHolder;
+	console.log(placeHolder);
     // var teste = document.getElementById("aqui");
     // var a = document.createTextNode(dados);
     // teste.appendChild(a);
@@ -44,17 +46,38 @@ async function getElementsById(){
 var form = document.getElementById('formulario');
 form.addEventListener('submit', function(e) {
 
-	var campo = document.getElementById('description');
-
+	var titlePost = document.getElementById('title').value;
+	var humorPost = document.getElementById('humorPost');
+	var descriptionPost = document.getElementById('description');
+	const title = titlePost.value;
+	// const options = {title: , description: descriptionPost.value, thumor: humorPost.value};
+	
+	// post(options);
     // alerta o valor do campo
-    alert(campo.value);
+    alert(title);
+    modal.style.display = "none";
 
     // impede o envio do form
-    e.preventDefault();
+    // e.preventDefault();
 });
 
+// function post(objeto){
 
-getElementsById();
+
+// 	(async () => {
+// 		const rawResponse = await fetch('http://localhost:8080/', {
+// 			method: 'POST',
+// 			headers: {
+// 				'Accept': 'application/json',
+// 				'Content-Type': 'application/json'
+// 			},
+// 			body: JSON.stringify(objeto)
+// 		});
+// 		const content = await rawResponse.json();
+
+// 		console.log(content);
+// 	})();
+// }
 getElements();
 
 // Get the modal
@@ -70,37 +93,39 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
-    modal.style.display = "block";
+	modal.style.display = "block";
 }
 
 btnBaloon.onclick = function(){
+	getElementsById();
 	modalBaloon.style.display = "block";
+	
 }
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
-    modalBaloon.style.display = "none";
+	modal.style.display = "none";
+	modalBaloon.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    	modalBaloon.style.display = "none";
-    }
+	if (event.target == modal) {
+		modal.style.display = "none";
+		modalBaloon.style.display = "none";
+	}
 }
 
 //modal auto-expand
-$(document)
-    .one('focus.autoExpand', 'textarea.autoExpand', function(){
-        var savedValue = this.value;
-        this.value = '';
-        this.baseScrollHeight = this.scrollHeight;
-        this.value = savedValue;
-    })
-    .on('input.autoExpand', 'textarea.autoExpand', function(){
-        var minRows = this.getAttribute('data-min-rows')|0, rows;
-        this.rows = minRows;
-        rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 16);
-        this.rows = minRows + rows;
-    });
+(document)
+.one('focus.autoExpand', 'textarea.autoExpand', function(){
+	var savedValue = this.value;
+	this.value = '';
+	this.baseScrollHeight = this.scrollHeight;
+	this.value = savedValue;
+})
+.on('input.autoExpand', 'textarea.autoExpand', function(){
+	var minRows = this.getAttribute('data-min-rows')|0, rows;
+	this.rows = minRows;
+	rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 16);
+	this.rows = minRows + rows;
+});

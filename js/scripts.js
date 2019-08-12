29e5698e7a819a6b96661677fd651fe9f174a44a
Robertson Souza <s2it_rsouza@uolinc.com>
2019-08-12 13:42:14 -0300

    Inserindo autor na estrutura do post

--- js/scripts.js
+++ js/scripts.js
@@ -47,8 +47,9 @@ form.addEventListener('submit', e => {
 	const title = document.getElementById('title').value;
 	const humor = document.getElementById('humorPost').value;
 	const description = document.getElementById('description').value;
+	const author = document.getElementById('author').value;
     
-    const data = { title, description, humor };
+    const data = { title, description, humor, author };
 	
 	post(data);
     getElements();
